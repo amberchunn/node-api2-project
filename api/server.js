cecc9a64ab59112bd93af1f0fc6cf245e3ebf3fe
Amber Chunn <a.77@me.com>
2021-02-14 21:11:25 -0500

    Added Create React App

--- api/server.js
+++ api/server.js
@@ -1,2 +1,16 @@
+const express = require("express");
+const postsRouter = require("./posts/posts-router");
+const cors = require("cors");
+
 // implement your server here
+
+const server = express();
+
+server.use(express.json());
+server.use(cors());
+
 // require your posts router and connect it here
+server.use(postsRouter);
+
+
+module.exports = server;
